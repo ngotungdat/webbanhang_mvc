35ac2795b3f47df14b30c59c00c55de52f275fa8
ngotungdat <ngotungdat1997@gmail.com>
2020-05-03 22:30:26 +0700

    update
    update3/5/2020

---
+++ WebBanHang/Areas/Administrator/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHang.Model;
+
+namespace WebBanHang.Areas.Administrator.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Administrator/Home
+        WebBanHangDbContext db = new WebBanHangDbContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string username,string password)
+        {
+            USER user = db.USERS.SingleOrDefault(x => x.UserName == username && x.PassWord == password && x.Allowed == 1);
+            if (user != null)
+            {
+                Session["userid"] = user.UserId;
+                Session["username"] = user.UserName;
+                Session["avatar"] = user.Avatar;
+                return RedirectToAction("Index","SanPham");
+            }
+            else
+            {
+                ViewBag.error = "Sai  tên đăng nhập hoặc mật khẩu!";
+                return View();
+            }
+        }
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebBanHang/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHang.Model;
+
+namespace WebBanHang.Controllers
+{
+    public class HomeController : Controller
+    {
+        WebBanHangDbContext db = new WebBanHangDbContext();
+        public ActionResult Index()
+        {
+            ViewBag.SanPhamMoi = db.SanPhams.OrderByDescending(x => x.NgayCapNhat).Take(4).ToList();
+            ViewBag.SanPhamBanChay = db.SanPhams.OrderByDescending(x => x.SoLuongBan).Take(4).ToList();
+            return View();
+        }
+
+        public ActionResult Detailes(string id)
+        {
+            SanPham sp = db.SanPhams.SingleOrDefault(x => x.MaSanPham == id);
+            if (sp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sp);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
